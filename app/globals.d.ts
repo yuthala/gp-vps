9581331cfe40e7e09d2960629bf7a290ca0e868a
Yuthala <dinapolousova@gmail.com>
2026-04-29 20:21:02 +0300

    fix: YD widget styles

---
+++ app/globals.d.ts
@@ -0,0 +1,5 @@
+// src/globals.d.ts
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type  Metadata  from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { inter, manrope } from "@/app/ui/fonts";
 import "@/app/globals.css";
