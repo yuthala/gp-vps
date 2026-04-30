af4207615833cf704d50bd94487232c7a3c3ffcb
Igor Polousov <igorpolousov@gmail.com>
2026-04-30 13:10:43 +0300

    --

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import  {NextConfig}  from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    turbopack: {
-      // Указывает текущую директорию как корень проекта
-      root: __dirname, 
-    },
-};
 
+}
 export default nextConfig;
