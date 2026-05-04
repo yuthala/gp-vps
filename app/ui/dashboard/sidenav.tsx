390a31925055831a528a9cf7f1f1d7485122a944
Yuthala <dinapolousova@gmail.com>
2026-05-04 21:10:30 +0300

    fix: YD container fix

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -45,7 +45,7 @@ export default function SideNav() {
           <button
             type="submit"
             disabled={isLoading}
-            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 disabled:opacity-50 md:flex-none md:justify-start md:p-2 md:px-3"
+            className="flex h-12 w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 disabled:opacity-50 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
