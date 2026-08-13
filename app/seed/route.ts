d768223d54cafab9800437bc2f36dcb4ea76cb62
Igor Polousov <igorpolousov@gmail.com>
2026-08-13 17:37:55 +0300

    + error corrections

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,7 @@
 import postgres from 'postgres';
 import { NextResponse } from 'next/server';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import bcrypt from 'bcrypt'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -25,35 +26,35 @@ async function getAuthorizedUser(req: Request) {
   return rows[0] || null;
 }
 
-// async function seedPersonalDataAgreements() {
-//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//   await sql`CREATE EXTENSION IF NOT EXISTS pgcrypto`;
+async function seedPersonalDataAgreements() {
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`CREATE EXTENSION IF NOT EXISTS pgcrypto`;
 
-//   await sql`
-//       CREATE TABLE IF NOT EXISTS customerspd (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       first_name VARCHAR(255) NOT NULL,
-//       last_name VARCHAR(255) NOT NULL,
-//       email TEXT NOT NULL UNIQUE,
-//       phone_number TEXT NOT NULL UNIQUE,
-//       );
-//   `;
-// }
+  await sql`
+      CREATE TABLE IF NOT EXISTS customerspd (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      first_name VARCHAR(255) NOT NULL,
+      last_name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      phone_number TEXT NOT NULL UNIQUE,
+      );
+  `;
+}
 
 async function seedPersonalDataAConsents() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`CREATE EXTENSION IF NOT EXISTS pgcrypto`;
 
-  // // Таблица клиентов (исправлена лишняя запятая перед закрывающей скобкой)
-  // await sql`
-  //     CREATE TABLE IF NOT EXISTS customerspd (
-  //     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  //     first_name VARCHAR(255) NOT NULL,
-  //     last_name VARCHAR(255) NOT NULL,
-  //     email TEXT NOT NULL UNIQUE,
-  //     phone_number TEXT NOT NULL UNIQUE
-  //     );
-  // `;
+  // Таблица клиентов (исправлена лишняя запятая перед закрывающей скобкой)
+  await sql`
+      CREATE TABLE IF NOT EXISTS customerspd (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      first_name VARCHAR(255) NOT NULL,
+      last_name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      phone_number TEXT NOT NULL UNIQUE
+      );
+  `;
 
   // Таблица фиксации согласий клиентов (Доказательная база для Роскомнадзора)
   await sql`
@@ -80,102 +81,102 @@ async function seedPersonalDataAConsents() {
 }
 
 
-// async function seedUsers() {
-//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//   await sql`
-//     CREATE TABLE IF NOT EXISTS users (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email TEXT NOT NULL UNIQUE,
-//       password TEXT NOT NULL
-//     );
-//   `;
+async function seedUsers() {
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    );
+  `;
 
-//   const insertedUsers = await Promise.all(
-//     users.map(async (user) => {
-//       const hashedPassword = await bcrypt.hash(user.password, 10);
-//       return sql`
-//         INSERT INTO users (id, name, email, password)
-//         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-//         ON CONFLICT (id) DO NOTHING;
-//       `;
-//     }),
-//   );
+  const insertedUsers = await Promise.all(
+    users.map(async (user) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      return sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+    }),
+  );
 
-//   return insertedUsers;
-// }
+  return insertedUsers;
+}
 
-// async function seedInvoices() {
-//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+async function seedInvoices() {
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-//   await sql`
-//     CREATE TABLE IF NOT EXISTS invoices (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       customer_id UUID NOT NULL,
-//       amount INT NOT NULL,
-//       status VARCHAR(255) NOT NULL,
-//       date DATE NOT NULL
-//     );
-//   `;
+  await sql`
+    CREATE TABLE IF NOT EXISTS invoices (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      customer_id UUID NOT NULL,
+      amount INT NOT NULL,
+      status VARCHAR(255) NOT NULL,
+      date DATE NOT NULL
+    );
+  `;
 
-//   const insertedInvoices = await Promise.all(
-//     invoices.map(
-//       (invoice) => sql`
-//         INSERT INTO invoices (customer_id, amount, status, date)
-//         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-//         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
-//   );
+  const insertedInvoices = await Promise.all(
+    invoices.map(
+      (invoice) => sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+        ON CONFLICT (id) DO NOTHING;
+      `,
+    ),
+  );
 
-//   return insertedInvoices;
-// }
+  return insertedInvoices;
+}
 
-// async function seedCustomers() {
-//   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+async function seedCustomers() {
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-//   await sql`
-//     CREATE TABLE IF NOT EXISTS customers (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email VARCHAR(255) NOT NULL,
-//       image_url VARCHAR(255) NOT NULL
-//     );
-//   `;
+  await sql`
+    CREATE TABLE IF NOT EXISTS customers (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      image_url VARCHAR(255) NOT NULL
+    );
+  `;
 
-//   const insertedCustomers = await Promise.all(
-//     customers.map(
-//       (customer) => sql`
-//         INSERT INTO customers (id, name, email, image_url)
-//         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-//         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
-//   );
+  const insertedCustomers = await Promise.all(
+    customers.map(
+      (customer) => sql`
+        INSERT INTO customers (id, name, email, image_url)
+        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+        ON CONFLICT (id) DO NOTHING;
+      `,
+    ),
+  );
 
-//   return insertedCustomers;
-// }
+  return insertedCustomers;
+}
 
-// async function seedRevenue() {
-//   await sql`
-//     CREATE TABLE IF NOT EXISTS revenue (
-//       month VARCHAR(4) NOT NULL UNIQUE,
-//       revenue INT NOT NULL
-//     );
-//   `;
+async function seedRevenue() {
+  await sql`
+    CREATE TABLE IF NOT EXISTS revenue (
+      month VARCHAR(4) NOT NULL UNIQUE,
+      revenue INT NOT NULL
+    );
+  `;
 
-//   const insertedRevenue = await Promise.all(
-//     revenue.map(
-//       (rev) => sql`
-//         INSERT INTO revenue (month, revenue)
-//         VALUES (${rev.month}, ${rev.revenue})
-//         ON CONFLICT (month) DO NOTHING;
-//       `,
-//     ),
-//   );
+  const insertedRevenue = await Promise.all(
+    revenue.map(
+      (rev) => sql`
+        INSERT INTO revenue (month, revenue)
+        VALUES (${rev.month}, ${rev.revenue})
+        ON CONFLICT (month) DO NOTHING;
+      `,
+    ),
+  );
 
-//   return insertedRevenue;
-// }
+  return insertedRevenue;
+}
 
 function forbiddenPage() {
   const html = `<!DOCTYPE html>
@@ -311,11 +312,11 @@ export async function GET(req: Request) {
     }
 
     const result = await sql.begin((sql) => [
-      // seedUsers(),
-      //seedCustomers()
-      // seedInvoices(),
-      // seedRevenue(),
-      //seedPersonalDataAgreements(),
+      seedUsers(),
+      seedCustomers(),
+      seedInvoices(),
+      seedRevenue(),
+      seedPersonalDataAgreements(),
       seedPersonalDataAConsents()
     ]);
 
