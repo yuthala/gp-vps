f5ae23eb1e624bf5ff4173606ed979360928daea
Igor Polousov <igorpolousov@gmail.com>
2026-08-16 12:54:27 +0300

    todo update

--- ToDo.ts
+++ ToDo.ts
@@ -1,2 +1,4 @@
 // 1. Перенести function forbiddenPage()  из seed/rout.ts в not-found.ts
 // 2. Поменять ссылку на редактирование товаров в dashboard
+// 3. Сделать доступ в dashboard только для sales@greenpato.ru
+// 4. Для пользователей сделать user-dashboard(заказы, чеки, урожаи-в будущем)
